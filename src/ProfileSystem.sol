d1cf76ceabfa0b2ea48d3afa1984c1ce99d1d908
ueyxxd <ethan.vuillard@epitech.eu>
2026-02-03 10:58:50 +0100

    exercice 2

--- src/ProfileSystem.sol
+++ src/ProfileSystem.sol
@@ -3,7 +3,9 @@ pragma solidity ^0.8.20;
 
 contract ProfileSystem {
     enum roleEnum {
-        GUEST, USER, ADMIN
+        GUEST,
+        USER,
+        ADMIN
     }
 
     struct UserProfile {
@@ -18,4 +20,37 @@ contract ProfileSystem {
     error UserAlreadyExists();
     error EmptyUsername();
     error UserNotRegistered();
+
+    modifier onlyRegistered() {
+        if (profiles[msg.sender].level == 0) {
+            revert UserNotRegistered();
+        }
+        _;
+    }
+
+    function createProfile(string calldata _Ethan) external {
+        if (bytes(_Ethan).length == 0) {
+            revert EmptyUsername();
+        }
+
+        if (profiles[msg.sender].level != 0) {
+            revert UserAlreadyExists();
+        }
+
+        profiles[msg.sender] = UserProfile(
+            _Ethan,
+            1,
+            roleEnum.USER,
+            block.timestamp
+        );
+    }
+
+    function levelUp() external onlyRegistered {
+        profiles[msg.sender].level += 1;
+        profiles[msg.sender].lastUpdated = block.timestamp;
+    }
+
+    event ProfileCreated(address indexed user, string username);
+    event LevelUp(address indexed user, uint256 newLevel);
+
 }
